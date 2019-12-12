01925f1f88b9388440a99d831a2532f1637c42b6
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-12-12 23:48:59 +0530

    updated filter focus

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,48 +5,14 @@ export default function Filters(props) {
     <div className="filters">
       <h4>Sizes:</h4>
       <div className="filter-sizes-div">
-        <input
-          onClick={() => props.handleFilters("XS")}
-          className="filter-sizes"
-          text=""
-          value="XS"
-        />
-        <input
-          onClick={() => props.handleFilters("S")}
-          className="filter-sizes"
-          text=""
-          value="S"
-        />
-        <input
-          onClick={() => props.handleFilters("M")}
-          className="filter-sizes"
-          text=""
-          value="M"
-        />
-        <input
-          onClick={() => props.handleFilters("ML")}
-          className="filter-sizes"
-          text=""
-          value="ML"
-        />
-        <input
-          onClick={() => props.handleFilters("L")}
-          className="filter-sizes"
-          text=""
-          value="L"
-        />
-        <input
-          onClick={() => props.handleFilters("XL")}
-          className="filter-sizes"
-          text=""
-          value="XL"
-        />
-        <input
-          onClick={() => props.handleFilters("XXL")}
-          className="filter-sizes"
-          text=""
-          value="XXL"
-        />
+        {["XS", "S", "M", "ML", "L", "XL", "XXL"].map(size => (
+          <input
+            onClick={() => props.handleFilters(size)}
+            className="filter-sizes"
+            text=""
+            value={size}
+          />
+        ))}
       </div>
     </div>
   );
